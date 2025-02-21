538ce61423243f49656eb0aaf2a8429afc79b11f
lorettaveiga <a00835854@tec.mx>
2025-02-20 23:22:18 -0600

    tarea

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Footer from "./components/Footer";
+import List from "./pages/List";
+import Add from "./components/Add";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ResponsiveAppBar from "./components/AppBar";
+import Login from "./pages/Login";
+
+function App() {
+  const [items, setItems] = useState([
+    { id: 1, name: "item1", price: 1 },
+    { id: 2, name: "item2", price: 2 },
+    { id: 3, name: "item3", price: 3 },
+  ]);
+  const [count, setCount] = useState(0);
+  const [isLogin, setIsLogin] = useState(false);
+  const sum = () => {
+    setCount(count + 1);
+  };
+  const resta = () => {
+    setCount(count - 1);
+  };
+  const add = (item) => {
+    item.id = items.length + 1;
+    setItems([...items, item]);
+  };
+  const del = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+  const login = (user) => {
+    if (user.username === "loretta" && user.password === "2004") {
+      setIsLogin(true);
+    }
+    return isLogin;
+  };
+  const setLogout = () => {
+    setIsLogin(false);
+  };
+  return (
+    <div>
+      <BrowserRouter>
+        {isLogin && <ResponsiveAppBar setLogout={setLogout} />}
+        <Routes>
+          <Route path="/" element={<Login login={login} />} />
+          <Route path="/add" element={<Add add={add} />} />
+          <Route
+            path="/items"
+            element={<List items={items} ondelete={del} />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* {count}
+      <Boton name={"suma"} click={sum} />
+      <Boton click={resta} name={"resta"} />
+      <Boton name={"mensaje"} click={() => alert("hola")} /> */}
+    </div>
+  );
+}
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <h1>Header</h1>
+      <Link to="/add">Añadir</Link>
+      <Link to="/items">Articulos</Link>
     </div>
   );
 };
